19c26d1c2aa8b64c51673a507c6d159a982dcd25
nje <arark057@gmail.com>
2025-01-22 13:05:08 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -10,12 +10,12 @@ import { getMessaging } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-
 
 // Firebase 초기화 (서비스 워커용)
 const firebaseConfig = {
-  apiKey: "AIzaSyC0jw2xwrWz5jW1qkfpjoIZdzePrs7aJsc",
-  authDomain: "test-messaging-acaae.firebaseapp.com",
-  projectId: "test-messaging-acaae",
-  storageBucket: "test-messaging-acaae.firebasestorage.app",
-  messagingSenderId: "207356196934",
-  appId: "1:207356196934:web:79dbb8793df26ebad9b247",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 //const app = initializeApp(firebaseConfig);
 //const app = firebase.initializeApp(firebaseConfig);
